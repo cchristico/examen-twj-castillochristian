abb44715c3aa0c034be0f4704e459f1b7473c23b
Christian Castillo <christian.castillo@epn.edu.ec>
2017-03-06 13:09:37 -0500

    Integracion sails y angular en consulta a c9.io Comit final final I hope :)

--- EquipoFE/src/app/services/master-url.service.ts
+++ EquipoFE/src/app/services/master-url.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
 export class MasterURLService {
   private _url:string;
   constructor() {
-    this._url="http://localhost:1337/";
-//this._url="https://c9.io/cchristico/prueba-twj-castillochristian";
+//    this._url="http://localhost:1337/";
+  this._url="https://examen-twj-castillochristian-cchristico.c9users.io";
   }
 get url():string{
     return this._url;
